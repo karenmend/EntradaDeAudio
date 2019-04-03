187aa678a9c74f1b491411b91b7c2e1cd627e449
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-04-03 11:46:42 -0700

    Agregar archivos de proyecto.

---
+++ Entrada/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using NAudio.Dsp;
+
+namespace Entrada
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WaveIn waveIn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LlenarComboDispositivos();
+        }
+        public void LlenarComboDispositivos()
+        {
+            for(int i= 0; i<WaveIn.DeviceCount; i++)
+            {
+                WaveInCapabilities capacidades = WaveIn.GetCapabilities(i);
+                cbDispositivo.Items.Add(capacidades.ProductName);
+            }
+            cbDispositivo.SelectedIndex = 0;
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn = new WaveIn();
+            //Formato de Audio
+            waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+            //Buffer
+            waveIn.BufferMilliseconds = 250;
+            
+
+
+        }
+    }
+}
