d6c043a642f0f2bd17a298586f7233666a0e8c6b
Karen Mendivil <karenlili14@gmail.com>
2019-04-05 12:30:16 -0700

    ya hace el recording y el slider se mueve.

--- Entrada/MainWindow.xaml.cs
+++ Entrada/MainWindow.xaml.cs
@@ -47,9 +47,39 @@ namespace Entrada
             waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
             //Buffer
             waveIn.BufferMilliseconds = 250;
-            
+
+            //¿Que hacer cuando hay muestras disponibles?
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            //Comienza a obtener muestras
+            waveIn.StartRecording();
+
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+            float acumulador = 0.0f;
+            for(int i=0; i<bytesGrabados; i += 2)
+            {
+                //Transformando 2  bytes separados en una muestra de 16 bits 
+                //1.- Toma el segundo byte y le antepone 8 0's al principio.
+                //2.- Hace un OR con el primer byte, al cual automaticamente se llenan 8 0's al final.
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+
+                float muestra32bits = (float)muestra / 327668.0f;
+                acumulador += Math.Abs(muestra32bits);
+            }
+            float promedio = acumulador / (bytesGrabados / 2.0f);
+            sldMicrofono.Value = (double)promedio;
 
 
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
         }
     }
 }
